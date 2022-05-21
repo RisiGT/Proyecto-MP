78025002fd594ee86ed6ce2709a73769fdc1016a
pcris <pcris@DESKTOP-QLKJK6F>
2022-05-21 18:53:18 +0200

    texturas

--- src/practicamp/PracticaMP.java
+++ src/practicamp/PracticaMP.java
@@ -6,13 +6,18 @@
 //Version 3.3
 package practicamp;
 
+import com.jtattoo.plaf.graphite.GraphiteLookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author PcCom
  */
 public class PracticaMP {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new GraphiteLookAndFeel());
         Multiplexor mult = new Multiplexor();
         mult.start();
     }
